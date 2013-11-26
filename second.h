87977cc5d8a69db907a86c4d6f42420ffd6e3a40
che-nikit <nikit.chernyh@gmail.com>
2013-11-26 20:12:41 +0700

    исправлено

--- second.h
+++ second.h
@@ -5,19 +5,25 @@ extern "C"
 
 class  CSECOND {
 public:
-        CSECOND(void);
-        CSECOND(char *fileName, int flag);
-        CSECOND operator+(CSECOND );
-        CSECOND operator-(CSECOND );
-        CSECOND operator*(CSECOND );
-        CSECOND operator/(CSECOND );
-        CSECOND operator%(CSECOND );
-        CSECOND operator^(CSECOND );
-        void b_output(char *fileN);
-        void output(char *fileN);
-        void PRINTOUT();
+	CSECOND(void);
+	CSECOND(char *fileName, int flag);
+	bool operator==(CSECOND );
+	bool operator!=(CSECOND );
+	bool operator<(CSECOND );
+	bool operator>(CSECOND );
+
+	CSECOND operator+(CSECOND );
+	
+	CSECOND operator-(CSECOND );
+	CSECOND operator*(CSECOND );
+	CSECOND operator/(CSECOND );
+	CSECOND operator%(CSECOND );
+	CSECOND operator^(CSECOND );
+	void b_output(char *fileN);
+	void output(char *fileN);
+	void PRINTOUT();
 private:
-        CSECOND(unsigned char *str, int s);
-        unsigned char *str_num;
-        int *sign;
+	CSECOND(unsigned char *str, int s);
+	unsigned char *str_num;
+	int *sign;
 };
